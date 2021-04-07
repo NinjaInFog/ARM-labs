4dd57d9dc23de6e62c9d19d1427375cc2d5e6934
Boleslav <borisvkotov@gmail.com>
2021-04-08 02:30:47 +0300

    completed lab2

--- labs/lab2/main.c
+++ labs/lab2/main.c
@@ -1,14 +1,4 @@
-//__asm void my_strcpy(const char *src, char *dst){
-//loop
-// 	LDRB r2, [r0]
-//	ADDS r0, #1 
-//	STRB r2, [r1] 
-//	ADDS r1, #1 
-//	CMP r2, #0 
-//	BNE loop 
-//	BX lr 
-//}
-
+#include "defines.h"
 __asm void my_strcpy(const char *src, char *dst){
 loop
  	LDRB r2, [r0]
@@ -25,36 +15,19 @@ loop2
 	BNE loop2
 	BX lr 
 }
-
-__asm void my_capitalize(char *str){
-cap_loop
-	LDRB r1, [r0] 
-	CMP r1, #'a'-1 
-	BLS cap_skip
-	CMP r1, #'z'
-	BHI cap_skip
-	SUBS r1,#32 
-	STRB r1, [r0] 
-cap_skip
-	ADDS r0, r0, #1 
-	CMP r1, #0 
-	BNE cap_loop 
-	BX lr 
-}
-
-
-__asm void Simple_add(){
-	
-}
-
-
 int main(void)
 {
 	const char a[] = "Hello world!";
-	//unsigned int a = 1, b = 2, d = 3, e = 4, f = 5, h = 0xfffffff1;
-	//volatile int c = 0;
+	const int arr_numb = 7;
+	const int sum_numb = 3;
+	uint32 summ[sum_numb] = {1, 0xfffffff1, 15};
+	uint32 arr[arr_numb] = {15, 0xfffffff6, 24, 0xfffffff7, 22, 0xfffffff9, 0xfffffffA};
+	volatile uint64 c = 0;
+	volatile uint64 max = 0;
 	char b[20];
+	int n = 5;
 	my_strcpy(a, b);
-	my_capitalize(b);
+	c = unsigned_sum(summ, sum_numb);
+	max = arr_max(arr, arr_numb);
 	while (1);
 }
